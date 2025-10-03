bffc79bfad19cfe3bd45a5150fef457c0dfd8b9f
jkaalekei <jkaalekei@gmail.com>
2025-10-03 16:27:32 -0500

    proyecto react practica carrito de compras guitarLA CAMBIO 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,11 +28,11 @@ export default function Header({ cart,removeFromToCart,incrementarQuantity, decr
           <div className="row justify-content-center justify-content-md-between">
             <div className="col-8 col-md-3">
               <a href="index.html">
-                <img
+                {/* <img
                   className="img-fluid"
                   src={`${import.meta.env.BASE_URL}/img/guitarra_12.jpg`}
                   alt="imagen logo"
-                />
+                /> */}
               </a>
             </div>
             <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
